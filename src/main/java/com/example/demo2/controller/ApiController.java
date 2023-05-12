efe47bc9de5b76282059b0889c0fe828b2301052
Mohammed Azam <azam145@gmail.com>
2023-05-12 03:28:37 +0100

    initial check in

--- src/main/java/com/example/demo2/controller/ApiController.java
+++ src/main/java/com/example/demo2/controller/ApiController.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +18,6 @@ import java.util.List;
 @RequestMapping("api/v1/book")
 public class ApiController {
 
-    @GetMapping
     public ResponseEntity<List<Bookdto>> getBook(String name) {
         Bookdto book = Bookdto.builder()
                 .name("somebook")
