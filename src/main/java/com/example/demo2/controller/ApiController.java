38304f569be79b6207952e6541669991daf49d1d
Mohammed Azam <azam145@gmail.com>
2023-05-12 03:49:05 +0100

    added dto.

--- src/main/java/com/example/demo2/controller/ApiController.java
+++ src/main/java/com/example/demo2/controller/ApiController.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ import java.util.List;
 @RequestMapping("api/v1/book")
 public class ApiController {
 
+    @GetMapping
     public ResponseEntity<List<Bookdto>> getBook(String name) {
         Bookdto book = Bookdto.builder()
                 .name("somebook")
